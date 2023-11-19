3422d7a66abb5aac2940f576b1dd42ae8ff85841
nazar.hevelenko <ocheretenko.s@gmail.com>
2023-11-19 17:26:28 +0200

    fix: don't use magic string for user repository

--- laboratory-work-5/backend/src/database/providers/user.provider.ts
+++ laboratory-work-5/backend/src/database/providers/user.provider.ts
@@ -1,7 +1,9 @@
 import { Provider } from '@nestjs/common';
 import { User } from '../entities/User';
 
+export const USER_REPOSITORY = 'USER_REPOSITORY';
+
 export const userProvider: Provider = {
-  provide: 'USER_REPOSITORY',
+  provide: USER_REPOSITORY,
   useValue: User,
 };
--- laboratory-work-5/backend/src/modules/user/user.service.ts
+++ laboratory-work-5/backend/src/modules/user/user.service.ts
@@ -1,10 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { User } from '../../database/entities/User';
+import { USER_REPOSITORY } from '../../database/providers/user.provider';
 
 @Injectable()
 export class UserService {
   constructor(
-    @Inject('USER_REPOSITORY') private readonly userRepository: typeof User,
+    @Inject(USER_REPOSITORY) private readonly userRepository: typeof User,
   ) {}
 
   async findAll(): Promise<User[]> {
